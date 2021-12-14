27fed89c4299b801b8ea46976db3a79a0213fb2e
sirnet <artemshomin@gmail.com>
2021-12-14 15:56:18 +0300

    Задание 2.1 Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-"use strict";
+const getRandomNumber = (min, max, numberCharacters) => {
+  if(min < 0 || max < 0) {
+    return -1;
+  }
+
+  if(max < min){
+    [min, max] = [max,min];
+  }
+
+  let random = Math.random() * (max - min + 1) + min;
+  random = random.toFixed(numberCharacters);
+  return random;
+
+};
+
+getRandomNumber(3,2,3);
