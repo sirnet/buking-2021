b88a58b8ffdb5658095d36d744d5e2878a376922
sirnet <artemshomin@gmail.com>
2021-12-16 19:08:38 +0300

    Добавил объекты

--- js/main.js
+++ js/main.js
@@ -1,3 +1,60 @@
+const AVATAR = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png',
+];
+
+const TITLES = [
+  'Продажа лучшей квартиры',
+  'Куплю квартиру',
+  'Вынос мебели',
+  'Продажа квартиры возле дерева',
+  'Сдам квартиру в центре'
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bunglalow'
+];
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const DESCRIPTIONS = [
+  'размеры комнаты, высота, ширина; просторно ли, светло',
+  'окна, их величина, оформление',
+  'стены и пол, их цвет',
+  'есть ли декор, каков стиль помещения'
+];
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+
+const SIMILAR_ADS_COUNT = 10;
+
+
 const getRandomNumber = (min, max, numberCharacters) => {
   if(min < 0 || max < 0) {
     return -1;
@@ -12,6 +69,16 @@ const getRandomNumber = (min, max, numberCharacters) => {
 
 };
 
+const getRandomArrayElement = (elements) => {
+  return elements[_.random(0, elements.length - 1)];
+};
+
+const  createAds = () => {
+  return author = {
+    avatar : getRandomArrayElement(AVATAR)
+  }
+};
+
+const infoAds = new Array(SIMILAR_ADS_COUNT).fill(null).map(() => createAds());
+console.log(infoAds);
 getRandomNumber(3,2,3);
-
-
